c3be94f9ac14081a063fce3be885d128d13bee69
Douglas Santos <dgstos94@gmail.com>
2023-12-07 17:38:12 -0300

    feat: configure StatusBar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import CustomerView from "./src/pages/Customer/CustomerView";
 import styled from "styled-components/native";
 
 export default function App() {
   return (
     <AppContainer>
+      <StatusBar
+        hidden={false}
+        animated={true}
+        barStyle="dark-content"
+        backgroundColor="white"
+      />
       <CustomerView />
     </AppContainer>
   );
