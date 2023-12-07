b90c7a957617f61d62ec6f4375c819d94603d8ca
Douglas Santos <dgstos94@gmail.com>
2023-12-07 16:26:30 -0300

    feat: create CustomerView structure

---
+++ src/pages/Customer/CustomerView/index.js
@@ -0,0 +1,43 @@
+export default function CustomerView() {
+  return (
+    <CustomerInfo>
+      <CustomerInfoNav>
+        <PageTitle>Nome do cliente</PageTitle>
+      </CustomerInfoNav>
+      <CustomerInfoContent>
+        <CustomerData>
+          <CustomerInfoTitle>CNPJ</CustomerInfoTitle>
+          <CustomerInfoValue>00000000</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Telefone</CustomerInfoTitle>
+          <CustomerInfoValue>949521984</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>CEP</CustomerInfoTitle>
+          <CustomerInfoValue>987651</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Estado</CustomerInfoTitle>
+          <CustomerInfoValue>SC</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Cidade</CustomerInfoTitle>
+          <CustomerInfoValue>Gabir</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Bairro</CustomerInfoTitle>
+          <CustomerInfoValue>Tapera</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Endereço</CustomerInfoTitle>
+          <CustomerInfoValue>Rua 2</CustomerInfoValue>
+        </CustomerData>
+        <CustomerData>
+          <CustomerInfoTitle>Número</CustomerInfoTitle>
+          <CustomerInfoValue>dasds</CustomerInfoValue>
+        </CustomerData>
+      </CustomerInfoContent>
+    </CustomerInfo>
+  );
+}
