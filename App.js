aa5d2d16a3b649873eb0b7e4d280be63ea231018
Douglas Santos <dgstos94@gmail.com>
2023-12-08 21:55:12 -0300

    feat: create routing to tabs

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import "react-native-gesture-handler";
 import { View, StatusBar } from "react-native";
 import styled from "styled-components/native";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import { CustomerForm, CustomerList, CustomerView } from "./src/pages";
 
-const Stack = createStackNavigator();
+const CustomerStack = createStackNavigator();
+
+function CustomerTabs() {
+  return (
+    <CustomerStack.Navigator>
+      <CustomerStack.Screen
+        name="Clientes"
+        component={CustomerList}
+        options={{
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+        }}
+      />
+      <CustomerStack.Screen
+        name="Cadastrar"
+        component={CustomerForm}
+        options={{ headerShown: false, presentation: "modal" }}
+      />
+      <CustomerStack.Screen
+        name="Detalhe"
+        component={CustomerView}
+        options={{ headerShown: false, presentation: "modal" }}
+      />
+    </CustomerStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const generateDisplay = (route) => {
+  const name = getFocusedRouteNameFromRoute(route);
+
+  const acceptedTabs = ["Clientes", "Produtos", "Pedidos", undefined];
+  const display = acceptedTabs.includes(name) ? "flex" : "none";
+  return display;
+};
 
 export default function App() {
   return (
@@ -16,26 +58,21 @@ export default function App() {
         barStyle="dark-content"
         backgroundColor="white"
       />
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Clientes"
-          component={CustomerList}
-          options={{
-            headerTitleAlign: "center",
-            headerShadowVisible: false,
-          }}
-        />
-        <Stack.Screen
-          name="Cadastrar"
-          component={CustomerForm}
-          options={{ headerShown: false, presentation: "modal" }}
-        />
-        <Stack.Screen
-          name="Detalhe"
-          component={CustomerView}
-          options={{ headerShown: false, presentation: "modal" }}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = "person-sharp";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarInactiveTintColor: "#D4D6DD",
+          tabBarStyle: {
+            display: generateDisplay(route),
+          },
+        })}
+      >
+        <Tab.Screen name="Customer" component={CustomerTabs} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
