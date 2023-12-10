1c237d2dbfe519a5aef8f9cea3011463da959b18
Douglas Santos <dgstos94@gmail.com>
2023-12-09 21:11:35 -0300

    style: create CloseIcon component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,11 +1,21 @@
 import { Image, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
+import Icon from "@expo/vector-icons/MaterialIcons";
 
 const BackIconContainer = styled(TouchableOpacity)`
   width: 20px;
   height: 20px;
   position: absolute;
   left: 0;
+`;
+
+const CloseIconContainer = styled(TouchableOpacity)`
+  z-index: 1;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 54px;
+  left: 24px;
 `;
 
 function BackIcon({ onPress }) {
@@ -16,4 +26,12 @@ function BackIcon({ onPress }) {
   );
 }
 
-export { BackIcon };
+function CloseIcon({ onPress }) {
+  return (
+    <CloseIconContainer onPress={onPress}>
+      <Icon name="close" size={20} color="#2F3036" />
+    </CloseIconContainer>
+  );
+}
+
+export { BackIcon, CloseIcon };
