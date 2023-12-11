b040e7fd7765d89c5ff61b15852514e845abc1b6
Douglas Santos <dgstos94@gmail.com>
2023-12-11 15:48:28 -0300

    ref: removing files

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { customerReducer } from "./reducers/customerReducer";
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-  customers: customerReducer,
-});
-
-export default rootReducer;
