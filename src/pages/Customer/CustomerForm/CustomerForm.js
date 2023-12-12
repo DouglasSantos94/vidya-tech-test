a615ea7969b54c09c8792267c9ddd6c196024f9e
Douglas Santos <dgstos94@gmail.com>
2023-12-12 18:55:35 -0300

    ref: remove unused imports

--- src/pages/Customer/CustomerForm/CustomerForm.js
+++ src/pages/Customer/CustomerForm/CustomerForm.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { ScrollView, Text } from "react-native";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import {
