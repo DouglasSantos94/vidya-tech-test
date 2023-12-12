b4ba642a077ede2b927f029bde4fb9717269ff04
Douglas Santos <dgstos94@gmail.com>
2023-12-12 20:00:47 -0300

    ref: change field type to number

--- src/schema/productFormSchema.js
+++ src/schema/productFormSchema.js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const productFormSchema = Yup.object().shape({
   productName: Yup.string().required("Nome do produto é obrigatório!"),
-  productPrice: Yup.string().required("Preço do produto é obrigatório!"),
+  productPrice: Yup.number().required("Preço do produto é obrigatório!"),
   productDescription: Yup.string().required(
     "Descrição do produto é obrigatória!"
   ),
